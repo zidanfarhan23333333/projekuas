447499b66aaa83c27ad8bde4def17510042c8e04
irsydmuhf <151486551+irsydmuhf@users.noreply.github.com>
2023-12-08 21:21:42 +0700

    Update Key.php

--- application/controllers/api/Key.php
+++ application/controllers/api/Key.php
@@ -5,6 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // This can be removed if you use __autoload() in config.php OR use Modular Extensions
 /** @noinspection PhpIncludeInspection */
 require APPPATH . '/libraries/REST_Controller.php';
+require_once FCPATH . 'vendor/autoload.php';
 
 // use namespace
 use Restserver\Libraries\REST_Controller;
