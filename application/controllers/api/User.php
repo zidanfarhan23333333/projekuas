54c68663576bd03630169f698c9251c5d72891e8
zidanfarhan23333333 <111355899+zidanfarhan23333333@users.noreply.github.com>
2023-12-11 08:12:42 +0700

    Update User.php

--- application/controllers/api/User.php
+++ application/controllers/api/User.php
@@ -5,7 +5,7 @@
     require_once FCPATH . 'vendor/autoload.php';
     use Restserver\Libraries\REST_Controller;
 
-    class user extends REST_Controller {
+    class User extends REST_Controller {
 
         function __construct($config = 'rest') {
             parent::__construct($config);
@@ -15,50 +15,77 @@
             $this->load->library('jwt');
         }
 
-        function index_get() 
-            $authorizationHeader = $this->input->get_request_header('Authorization', true);
+        function validate()
+     {
+        $this->form_validation->set_rules('username', 'Username', 'required|trim');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+     }
 
-            if(empty($authorizationHeader) || $this->jwt->decode($authorizationHeader) === false) {
-                return $this->response(
+        
+        function index_get() 
+        {
+            $id = $this->get('id');
+            if($id == '') {
+                $data = $this->M_User->fetch_all();
+            } else {
+                $data = $this->M_User->fetch_single($id);
+            }
+            $this->response($data, 200);
+        }
+
+        function is_login() {
+            $authorizationHeader = $this->input->get_request_header('Authorization', true);
+    
+            if (empty($authorizationHeader) || $this->jwt->decode($authorizationHeader) === false) {
+                $this->response(
                     array(
                         'kode' => '401',
                         'pesan' => 'signature tidak sesuai',
                         'data' => []
                     ), '401'
-                );
-            }
-            $id = $this->get('id');
-            if($id == '') {
-              $data = $this->M_Detail->get_all();
-            } else{
-              $data = $this->M_Detail->get_by_id($id);
-            }
-            $this->response($data, 200)
-        function index_post() {
-            $username = $this->input->post('username');
-            $password = $this->input->post('password');
-
-            $check = $this->M_User->check_data($username, $password);
-
-            if($check == false) {
-                $error = array(
-                    'status' => 'fail',
-                    'message' => 'Kesalahan kresidensial',
-                    'status_code' => 401
                 );
-
-                return $this->response($error);
+                return false;
             }
+    
+            return true;
+        }
 
+        function index_post()
+        {
+
+            $this -> validate();
+
+            if ($this->form_validation->run() === FALSE) {
+                $error = $this->form_validation->error_array();
+                $response = array(
+                    'status_code' => 502,
+                    'message' => $error
+                );
+                return $this->response($response, 502);
+            }
+    
+            $username = $this->input->post('username');
+            $password = md5($this->input->post('password'));
+    
+            $user = $this->M_User->cek_login($username, $password);
+    
+            if (!$user) {
+                $response = array(
+                    'status_code' => 401,
+                    'message' => 'Invalid email or password'
+                );
+                return $this->response($response, 401);
+            }
+    
             $token = $this->jwt->encode($username, $password);
-
-            $response = array (
-                'status' => 'success',
-                'message' => 'login was successful',
+    
+            $response = array(
                 'status_code' => 200,
+                'message' => 'Login successful',
+                'user_data' => $user,
                 'token' => $token
             );
-            
+    
             return $this->response($response);
         }
     }
